f86badd10c2d840826bb6e6bbda109844f821d74
parimal <parimalie47@gmail.com>
2020-01-28 12:32:06 +0800

    Testing project

---
+++ CollectionDemo.java
@@ -0,0 +1,18 @@
+
+public class CollectionDemo {
+	public static void main(String[] args) {
+		for(int i = 0; i<=10; i++) {
+			System.out.println("  dsfrfwe  ");
+		}
+	}
+}
+
+class Customoer {
+
+}
+
+class student {
+
+}
+
+
